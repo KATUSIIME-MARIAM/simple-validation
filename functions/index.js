c514182889fe59aa6399805507aa1cd950783c7c
KATUSIIME-MARIAM <149708416+KATUSIIME-MARIAM@users.noreply.github.com>
2024-02-07 14:17:23 +0300

    Add files via upload
    Write a JavaScript function that takes two parameters a simple email password validation. if the provided is "yourstudentname@gmail.com"  and the password is "yourregistrationnumber@2024", the function should log a message saying "user email is [email], login successfully!" otherwise it should log "incorrect user credentials!"

---
+++ functions/index.js
@@ -0,0 +1,13 @@
+//console.log("Hello Mariam")
+
+//function for valid user crudential
+function validCredentials(email,password){
+    if(email==='katusiimemariam@gmail.com'&& password ==='22/U/6101@2024'){
+        console.log(`user email is ${email},Login successful`);
+    }else{
+        console.log('Incorrect user crudential')
+
+    }
+}
+//valid crudentials
+validCredentials('katusiimemariam@gmail.com','22/U/6101@2024')
